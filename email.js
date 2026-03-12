0182981a35d97e1c1bed52cd1faccc93733bf6eb
zduka <zduka@outlook.com>
2026-03-12 22:36:11 +0100

    2026 update, long overdue

--- email.js
+++ email.js
@@ -1,6 +1,6 @@
-var a = "majpetr";
+var a = "peta.maj82";
 var b = "@";
-var c = "fit.cvut.cz";
+var c = "gmail.com";
 (function(email) {
     document.write("<a href=\"mailto:" + email + "\">" + email + "</a>");
 })(a + b + c)
